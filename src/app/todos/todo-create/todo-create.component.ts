485021e9dc876965c3dd1fee12d12c3d03a21eb3
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-12 01:50:11 +0530

    added image preview in browser before uploading at server method-1

--- src/app/todos/todo-create/todo-create.component.ts
+++ src/app/todos/todo-create/todo-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { take, map } from 'rxjs/operators';
 import { TodoService } from './../../../service/todo.service';
 import { AlertService } from './../../../service/alert.service';
@@ -18,9 +19,12 @@ export class TodoCreateComponent implements OnInit {
   private todoData: Todo;
   isLoading = false;
 
+  imagePreview: string;
+
   constructor(private route: ActivatedRoute, 
               private todoService: TodoService, 
               private fb: FormBuilder,
+              private sanitizer: DomSanitizer,
               private alertService: AlertService,
               private router: Router) {
                
@@ -44,7 +48,8 @@ export class TodoCreateComponent implements OnInit {
   initailizeForm() {
     this.todoForm = this.fb.group({
       title: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      content: new FormControl('', Validators.required)
+      content: new FormControl('', Validators.required),
+      image: new FormControl(null, Validators.required)
     });
   }
 
@@ -84,6 +89,17 @@ export class TodoCreateComponent implements OnInit {
   prefillForm(data: Todo) {
     this.todoForm.controls.title.setValue(data?.title);
     this.todoForm.controls.content.setValue(data?.content);
+  }
+
+  fileChangeEvent(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.todoForm.controls.image.setValue(file);
+    console.log(URL.createObjectURL(file));
+    this.imagePreview = URL.createObjectURL(file);
+  }
+
+  getSantizeUrl(url : string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
   get title() {
